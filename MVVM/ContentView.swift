fcbb21a9d3544e3be2cd61b177df3f01b036461e
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-17 21:54:26 +0530

    Initial Commit

---
+++ MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM
+//
+//  Created by manvendu pathak  on 17/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by manvendu pathak  on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
